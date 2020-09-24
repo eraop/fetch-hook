9b2458ebb6d1fb4b0eee016e320b99000bfa721e
Weiyi <493214262@qq.com>
2020-09-24 16:52:54 +0800

    兼容处理

--- src/fetchhook.js
+++ src/fetchhook.js
@@ -4,30 +4,37 @@
  * source code: https://github.com/eraop/fetch-hook
  **/
 module.exports = function (ob) {
-    var realFetch = "RealFetch"
-    ob.hookFetch = function (proxy) {
-        // Avoid double hook
-        window[realFetch] = window[realFetch] || fetch
-        // 拦截原始的fetch方法 
-        window.fetch = function (url, opts) {
-            // fetch url拦截
-            if (typeof proxy.urlHook === "function") {
-                url = proxy.urlHook.call(this, url)
-                console.log(url)
-            }
-            // fetch options拦截
-            if (typeof proxy.optionsHook === "function") {
-                opts = proxy.optionsHook.call(this, opts)
-                console.log(opts)
-            }
-            // 定义新的fetch方法，封装原有的fetch方法
-            return window[realFetch](url, opts)
-        }
+  var realFetch = 'RealFetch'
+  ob.hookFetch = function (proxy) {
+    // Avoid double hook
+    if (window[realFetch] == undefined || window[realFetch] == null) {
+      if (typeof fetch == 'function') {
+        window[realFetch] = fetch
+      } else {
+        return false
+      }
     }
+    window[realFetch] = window[realFetch] || fetch
+    // 拦截原始的fetch方法
+    window.fetch = function (url, opts) {
+      // fetch url拦截
+      if (typeof proxy.urlHook === 'function') {
+        url = proxy.urlHook.call(this, url)
+        // console.log(url)
+      }
+      // fetch options拦截
+      if (typeof proxy.optionsHook === 'function') {
+        opts = proxy.optionsHook.call(this, opts)
+        // console.log(opts)
+      }
+      // 定义新的fetch方法，封装原有的fetch方法
+      return window[realFetch](url, opts)
+    }
+  }
 
-    // Cancel hook
-    ob.unHookFetch = function () {
-        if (window[realFetch]) fetch = window[realFetch];
-        window[realFetch] = undefined;
-    }
+  // Cancel hook
+  ob.unHookFetch = function () {
+    if (window[realFetch]) fetch = window[realFetch]
+    window[realFetch] = undefined
+  }
 }
